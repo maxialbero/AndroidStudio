8504d3f1c34b4d60d320913fa7d0c5f68471a460
maxialbero <m.maffez@gmail.com>
2022-06-22 16:25:45 +0200

    Ultime modifiche mie, prove di vari layout... app non funzionante da sistemare activity_main

--- MyCloset/app/src/main/java/com/example/mycloset/MainActivity.java
+++ MyCloset/app/src/main/java/com/example/mycloset/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.classes.objects.Item;
 import com.classes.utility.DB;
@@ -27,4 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         ChatAdapter chatAdapter = new ChatAdapter(this, list);
     }
+
+//    Button button = (Button) findViewById(R.id.Social);
+//    button.setOnClickListener(new View.OnClickListener() {
+//        public void onClick(View v) {
+//            // Do something in response to button click
+//        }
+//    });
 }
