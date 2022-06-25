5cde51a6560540872abb87167d11daf816eea229
Lorenzo <lorenzo.rossi-4@studenti.unitn.it>
2022-06-25 18:31:28 +0200

    DB INSERT

--- MyCloset/app/src/main/java/com/example/mycloset/MainActivity.java
+++ MyCloset/app/src/main/java/com/example/mycloset/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.mycloset;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.classes.utility.DB;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    DB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //setContentView(R.layout.social_view);
 
-        /*recyclerView = findViewById(R.id.recView);
+        db = new DB("", "", "");
 
-        DB db = new DB("", "", "");
+        db.connect();
+
+        /*recyclerView = findViewById(R.id.recView);
 
         ArrayList<Item> list = db.SELECT("");
         ChatAdapter chatAdapter = new ChatAdapter(this, list);
@@ -27,5 +37,25 @@ public class MainActivity extends AppCompatActivity {
                 // Do something in response to button click
             }
         });*/
+
+    }
+
+    public void save(View view) {
+        TextView username = (TextView) findViewById(R.id.txtUsername);
+        EditText t = (EditText) findViewById(R.id.editUsername);
+        String text = t.getText().toString();
+
+        HashMap<String, Object> json = new HashMap<>();
+
+        json.put("Nome", text);
+
+        String debug;
+        if(!text.isEmpty()) {
+            db.INSERT("Utente", json);
+            debug = "Inserito";
+        } else {
+            debug = "Non inserito";
+        }
+        Log.d("DEBUG", debug);
     }
 }
